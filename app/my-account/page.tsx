4d2aeded5dea1a128aad4f08bab7fada3de64504
surysagar <surysagar@gmail.com>
2025-12-21 13:02:37 +0530

    new

--- app/my-account/page.tsx
+++ app/my-account/page.tsx
@@ -359,6 +359,10 @@ export default function MyAccount() {
                         </div>
                     </div>
 
+                    {/* Distributor Details */}
+                    <DistributorDetails data={dashboardData.distributor} />
+
+
                     {/* Quick Actions */}
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
 
